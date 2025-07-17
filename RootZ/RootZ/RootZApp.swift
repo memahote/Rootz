fc874b7746b13da57f7f634e56db69fce6a109c5
memahote <emahotenm@gmail.com>
2025-07-17 11:20:19 +0200

    initial push

---
+++ RootZ/RootZ/RootZApp.swift
@@ -0,0 +1,17 @@
+//
+//  RootZApp.swift
+//  RootZ
+//
+//  Created by Mounir Emahoten on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct RootZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RootZ/RootZTests/RootZTests.swift
@@ -0,0 +1,17 @@
+//
+//  RootZTests.swift
+//  RootZTests
+//
+//  Created by Mounir Emahoten on 17/07/2025.
+//
+
+import Testing
+@testable import RootZ
+
+struct RootZTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
