bff7e10f5f70bd5622c0cf6c40d41b3c17733c5e
Mounirbl <mounir.bouallel@outlook.fr>
2025-07-25 17:15:27 +0200

    Merge Conflit User

--- RootZ/RootZ/Auth/Model/User.swift
+++ RootZ/RootZ/Auth/Model/User.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-struct User : Identifiable {
-    let id = UUID()
-    let name : String
-    let email : String
-    let password : String
-}
+//struct User : Identifiable {
+//    let id = UUID()
+//    let name : String
+//    let email : String
+//    let password : String
+//}
