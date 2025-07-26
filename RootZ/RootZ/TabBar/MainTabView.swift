97475b3561c5e79e0e0d8dd6b8fbd0fbf17f021b
memahote <emahotenm@gmail.com>
2025-07-26 22:30:10 +0200

    tab bar v1 added

---
+++ RootZ/RootZ/TabBar/MainTabView.swift
@@ -0,0 +1,53 @@
+//
+//  MainTabView.swift
+//  RootZ
+//
+//  Created by Mounir Emahoten on 18/07/2025.
+//
+
+import SwiftUI
+
+enum Tab {
+    case story, quest, ranking, profil
+}
+
+struct MainTabView: View {
+    @State private var viewModel = AppViewModel()
+    @State private var selectedTab: Tab = .story
+
+    var body: some View {
+        ZStack {
+            Group {
+                switch selectedTab {
+                case .story:
+                    NavigationStack {
+                        storymode(appViewModel: viewModel)
+                    }
+                case .quest:
+                    NavigationStack {
+                        QuestView()
+                    }
+                case .ranking:
+                    NavigationStack {
+                        PrincipalView()
+                    }
+                case .profil:
+                    NavigationStack {
+                        
+                    }
+                }
+            }
+            
+
+            CustomTabBar(selectedTab: $selectedTab, tabColor: viewModel.selectedCulture.buttonColor, tabAccentColor: viewModel.selectedCulture.backgroundColor)
+           
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
+
+
+
