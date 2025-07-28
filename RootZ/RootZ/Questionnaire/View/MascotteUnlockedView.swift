370a4da7fcc46527f58991f865e4890258cda02d
Nicodeur <codewithnico@icloud.com>
2025-07-28 17:15:31 +0200

    New animation ajustement onboarding

--- RootZ/RootZ/Questionnaire/View/MascotteUnlockedView.swift
+++ RootZ/RootZ/Questionnaire/View/MascotteUnlockedView.swift
@@ -14,21 +14,29 @@ struct MascotteUnlockedView: View {
         ZStack {
             Color(.backgroundDefault)
                 .ignoresSafeArea()
-
+            
             VStack(spacing: 20) {
-
-                // Mascotte affichée selon la culture
-                if let mascotteName = viewModel.selectedCulture()?.mascott {
-                    Image(mascotteName)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 300)
-                } else {
-                    Image(.planete1)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 200)
+                Spacer()
+                ZStack {
+                   
+                    // Mascotte affichée selon la culture
+                    if let mascotteName = viewModel.selectedCulture()?.mascott {
+                        Image(mascotteName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 300)
+                    } else {
+                        Image(.planete1)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                    }
+                    
+                    LottieView(name: "TapBurst", loopMode: .playOnce)
+                        .frame(width: 400, height: 350)
+                        .allowsHitTesting(false)
                 }
+              
 
                 Text("Bravo ! Tu as débloqué un nouveau compagnon pour ton avatar !")
                     .font(.custom("Baloo 2", size: 22))
@@ -55,5 +63,10 @@ struct MascotteUnlockedView: View {
     }
 }
 
-
+struct MascotteUnlockedView_Previews: PreviewProvider {
+    static var previews: some View {
+        MascotteUnlockedView(viewModel: OnboardingViewModel())
+            .previewDevice("iPhone 15 Pro")
+    }
+}
 
