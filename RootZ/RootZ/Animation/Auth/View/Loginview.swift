c228275c24aa14bd6cbf687fb9109145e140cb04
Nicodeur <codewithnico@icloud.com>
2025-07-29 15:56:23 +0200

    NEW ANIMATION LOTTIE / Onboarding mis à jours

---
+++ RootZ/RootZ/Animation/Auth/View/Loginview.swift
@@ -0,0 +1,28 @@
+//
+//  Loginview.swift
+//  RootZ
+//
+//  Created by Mounir Emahoten on 25/07/2025.
+//
+
+import SwiftUI
+
+struct Loginview: UIViewControllerRepresentable {
+    
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let loginVC = ViewController()
+       
+        return UINavigationController(rootViewController: loginVC)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+}
+
+#Preview {
+    Loginview()
+        .ignoresSafeArea()
+}
